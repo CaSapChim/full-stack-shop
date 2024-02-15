3d1553faa0d97967ec9882b53699b95d93070d2c
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-02-15 20:17:09 +0700

    👚 Fix: Responsive

--- client/src/Page/Home/Components/Footer.tsx
+++ client/src/Page/Home/Components/Footer.tsx
@@ -6,12 +6,14 @@ export const Footer = memo(() => {
       <div className="px-6">
         <div className="border-t-[1px] border-t-slate-900/10 py-5"></div>
         <div className="border-t-[1px] border-t-slate-900/10 py-6">
-          <div className="flex flex-col gap-6 md:flex-row md:items-center md:justify-between">
+          <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
             <div>
-              <h4 className="font-semibold text-slate-900">Theo dõi bản tin của chúng tôi</h4>
+              <h4 className="font-semibold text-slate-900">
+                Theo dõi bản tin của chúng tôi
+              </h4>
               <p className="mt-2 text-sm text-slate-600">
-                Tin tức và tài nguyên mới nhất được gửi tới mail đến
-                của bạn hàng tuần
+                Tin tức và tài nguyên mới nhất được gửi tới mail đến của bạn
+                hàng tuần
               </p>
             </div>
             <div className="flex h-9 gap-3 text-sm">
@@ -27,7 +29,7 @@ export const Footer = memo(() => {
           </div>
         </div>
         <div className="border-t-[1px] border-t-slate-900/10 py-5">
-          <div className="flex flex-col justify-center text-sm text-slate-600 md:flex-row md:justify-between ">
+          <div className="flex flex-col text-center text-sm text-slate-600 lg:flex-row lg:justify-between ">
             <p>© 2020 Your Company, Inc. All rights reserved.</p>
             <p>qưertyuiosdfghjkl</p>
           </div>
--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -1,5 +1,5 @@
 export const Sidebar = () => {
   return (
-    <aside className="min-h-screen w-32 translate-x-[-100%] bg-slate-50 p-4 md:translate-x-0"></aside>
+    <aside className="hidden min-h-screen w-[230px] translate-x-[-100%] bg-slate-900 p-4 md:flex md:translate-x-0"></aside>
   );
 };
